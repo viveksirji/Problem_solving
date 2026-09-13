5521f21e74d363cfdf82e5300d81f91c284943b1
Vivek Pandey <vivekpandeygtechtrainer@gmail.com>
2026-09-13 22:54:50 +0530

    Time: 0 ms (100%), Space: 19.4 MB (19.1%) - LeetHub

---
+++ 0258-add-digits/0258-add-digits.py
@@ -0,0 +1,11 @@
+class Solution:
+    def addDigits(self, num: int) -> int:
+        while num>9:
+            s=0
+            while num>0:
+                s+=num%10
+                num//=10
+            num=s
+        return num
+
+        
