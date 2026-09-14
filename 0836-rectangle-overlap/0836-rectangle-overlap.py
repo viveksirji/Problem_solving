cfde9705850e06be4de9c8e33376c263dadf28f2
Vivek Pandey <vivekpandeygtechtrainer@gmail.com>
2026-09-14 11:13:53 +0530

    Time: 0 ms (100%), Space: 19.3 MB (59.18%) - LeetHub

--- 0836-rectangle-overlap/0836-rectangle-overlap.py
+++ 0836-rectangle-overlap/0836-rectangle-overlap.py
@@ -14,3 +14,15 @@ class Solution:
             return False
 
         return True
+
+
+#  '''
+#  class Solution:
+#     def isRectangleOverlap(self, rec1: list[int], rec2: list[int]) -> bool:
+#         return not (
+#             rec2[0] >= rec1[2] or
+#             rec2[2] <= rec1[0] or
+#             rec2[1] >= rec1[3] or
+#             rec2[3] <= rec1[1]
+#         )
+#         '''
