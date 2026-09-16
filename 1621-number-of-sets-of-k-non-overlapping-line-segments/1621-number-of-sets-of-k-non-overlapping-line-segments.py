534c6e36bae79adac28167d63818f42987bf84c2
Vivek Pandey <vivekpandeygtechtrainer@gmail.com>
2026-09-16 21:48:37 +0530

    Time: 3 ms (63.79%), Space: 19.3 MB (86.21%) - LeetHub

--- 1621-number-of-sets-of-k-non-overlapping-line-segments/1621-number-of-sets-of-k-non-overlapping-line-segments.py
+++ 1621-number-of-sets-of-k-non-overlapping-line-segments/1621-number-of-sets-of-k-non-overlapping-line-segments.py
@@ -1,6 +1,31 @@
-from math import comb
-
 class Solution:
     def numberOfSets(self, n: int, k: int) -> int:
 
-        return comb(n + k - 1, 2 * k) % (10**9 + 7)
+        MOD = 10**9 + 7
+
+        N = n + k - 1
+        R = 2 * k
+
+        result = 1
+
+        for i in range(1, R + 1):
+            result = result * (N - i + 1) // i
+
+        return result % MOD
+
+
+
+
+
+
+
+
+
+
+
+# from math import comb
+
+# class Solution:
+#      def numberOfSets(self, n: int, k: int) -> int:
+
+#          return comb(n + k - 1, 2 * k) % (10**9 + 7)
